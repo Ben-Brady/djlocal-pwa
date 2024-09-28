1544fd5a4a4c22d5f6369e3514fa57b448a79591
Ben Brady <benbrady04.bb@gmail.com>
2024-09-28 17:35:27 +0100

    feat(VolumeSlider): Updated to use range

--- src/components/elements/VolumeButton.tsx
+++ src/components/elements/VolumeButton.tsx
@@ -3,8 +3,8 @@ import IconButton from "./IconButton";
 import { faVolumeHigh, faVolumeLow, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 import { useMemo, useRef } from "preact/hooks";
 import { useHover } from "@/hooks/useHover";
-import { percent } from "@/lib/utils";
 import { useHolding } from "@/hooks/useHolding";
+import classNames from "classnames";
 
 export const VolumeButton: FC<{
     volume: number;
@@ -14,52 +14,39 @@ export const VolumeButton: FC<{
     unmute: () => void;
 }> = ({ volume, muted, setVolume, mute, unmute }) => {
     const containerRef = useRef<HTMLDivElement>(null);
-    const volumeRef = useRef<HTMLDivElement>(null);
+    const rangeRef = useRef<HTMLInputElement>(null);
+
     const icon = useMemo(() => {
-        if (muted) return faVolumeMute;
+        if (muted || volume === 0) return faVolumeMute;
         if (volume < 0.2) return faVolumeLow;
         return faVolumeHigh;
-    }, [volume]);
+    }, [muted, volume]);
 
     const hovering = useHover(containerRef.current);
-    const holding = useHolding(containerRef.current);
+    const holding = useHolding(rangeRef.current);
 
+    const rangeVisible = useMemo(() => hovering || holding, [hovering, holding]);
     return (
         <div ref={containerRef} class="flex-row items-center gap-4 pr-2">
             <IconButton icon={icon} class="size-8" onClick={muted ? unmute : mute} />
-            {(hovering || holding) && (
-                <div
-                    class="animate-in-width relative h-1.5 w-20 rounded-lg bg-black sm:w-24"
-                    role="button"
-                    ref={volumeRef}
-                    onMouseMove={(ev) => {
-                        if (!holding) return;
-                        console.log(ev);
-                        const width = ev.currentTarget.clientWidth;
-                        const percent = (ev.layerX / width) ** 3;
-                        setVolume(percent);
-                    }}
-                    onTouchMove={(ev) => {
-                        if (!holding) return;
-                        console.log(ev);
-                        const width = ev.currentTarget.clientWidth;
-                        const percent = (ev.targetTouches[0].clientX / width) ** 3;
-                        setVolume(percent);
-                    }}
-                    onClick={(ev) => {
-                        const width = ev.currentTarget.clientWidth;
-                        const percent = (ev.layerX / width) ** 3;
-                        setVolume(percent);
-                    }}
-                >
-                    <div
-                        class="animate-in-width duration-25 relative h-full rounded-lg bg-accent transition-all ease-linear"
-                        style={{ width: percent(Math.cbrt(volume)) }}
-                    >
-                        <div class="absolute right-0 top-1/2 size-4 -translate-y-1/2 translate-x-1/2 rounded-full bg-accent" />
-                    </div>
-                </div>
-            )}
+            <input
+                class={classNames("h-full accent-accent transition-all", {
+                    "w-24": rangeVisible,
+                    "opacity-0 w-0": !rangeVisible,
+                })}
+                name="volume"
+                aria-label="Volume"
+                type="range"
+                ref={rangeRef}
+                min={0}
+                step={0.01}
+                max={1}
+                value={Math.cbrt(volume)}
+                onChange={(ev) => {
+                    const percent = parseFloat(ev.currentTarget.value);
+                    setVolume(percent ** 3);
+                }}
+            />
         </div>
     );
 };
