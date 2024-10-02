4276067422aa53798a2646c87e9b69fcafbaa3f4
Ben Brady <benbrady04.bb@gmail.com>
2024-10-02 19:31:56 +0100

    feat: added download/update buttons

--- src/components/extensive/Navbar.tsx
+++ src/components/extensive/Navbar.tsx
@@ -1,20 +1,50 @@
+import { useDownloadPrompt } from "@/hooks/useDownloadPrompt";
+import { useUpdatePrompt } from "@/hooks/useUpdatePrompt";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faFileAudio, faList, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+    faArrowsRotate,
+    faDownload,
+    faFileAudio,
+    faList,
+    faPlayCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "preact/compat";
 
-const Navbar: FC = () => (
-    <footer class="w-full border-t-2 border-t-background bg-tertiary flex-center">
-        <nav class="contents">
-            <NavbarIcon icon={faFileAudio} text="Library" href="/" />
-            <NavbarIcon icon={faPlayCircle} text="Queue" href="/" />
-            <NavbarIcon icon={faList} text="Playlists" href="/" />
-        </nav>
-    </footer>
-);
+const Navbar: FC = () => {
+    const updatePrompt = useUpdatePrompt();
+    const downloadPrompt = useDownloadPrompt();
+    return (
+        <footer class="w-full border-t-2 border-t-background bg-tertiary flex-center">
+            <nav class="contents">
+                <NavbarIcon icon={faFileAudio} text="Library" href="/" />
+                <NavbarIcon icon={faPlayCircle} text="Queue" href="/" />
+                <NavbarIcon icon={faList} text="Playlists" href="/" />
+                {downloadPrompt ?
+                    <NavbarIcon icon={faDownload} text="Install" callback={downloadPrompt} />
+                : updatePrompt ?
+                    <NavbarIcon icon={faArrowsRotate} text="Update" callback={updatePrompt} />
+                :   null}
+            </nav>
+        </footer>
+    );
+};
 
-const NavbarIcon: FC<{ href: string; icon: IconProp; text: string }> = ({ href, icon, text }) => (
-    <a className="size-full flex-col items-center justify-end gap-1 py-1.5" href={href}>
+const NavbarIcon: FC<{ icon: IconProp; text: string; href?: string; callback?: () => void }> = ({
+    href,
+    icon,
+    text,
+    callback,
+}) => (
+    <a
+        className="size-full flex-col items-center justify-end gap-1 py-1.5"
+        href={href}
+        onClick={ev => {
+            if (!callback) return;
+            ev.preventDefault();
+            callback();
+        }}
+    >
         <div class="size-5">
             <FontAwesomeIcon icon={icon} className="text-primary" />
         </div>
