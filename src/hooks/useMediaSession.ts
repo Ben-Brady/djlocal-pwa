28e086471141a208e8a56e58af60670e4fbeb803
Ben Brady <benbrady04.bb@gmail.com>
2024-10-02 22:06:49 +0100

    feat: added temporary album artwork

--- src/hooks/useMediaSession.ts
+++ src/hooks/useMediaSession.ts
@@ -11,6 +11,14 @@ export const useMediaSessionSong = (song: undefined | SongMetadata) => {
                 title: title,
                 album: "",
                 artist: artist ?? "Unknown Artist",
+                artwork: [
+                    { src: "https://dummyimage.com/96x96", sizes: "96x96", type: "image/png" },
+                    { src: "https://dummyimage.com/128x128", sizes: "128x128", type: "image/png" },
+                    { src: "https://dummyimage.com/192x192", sizes: "192x192", type: "image/png" },
+                    { src: "https://dummyimage.com/256x256", sizes: "256x256", type: "image/png" },
+                    { src: "https://dummyimage.com/384x384", sizes: "384x384", type: "image/png" },
+                    { src: "https://dummyimage.com/512x512", sizes: "512x512", type: "image/png" },
+                ],
             });
         }
     }, [song]);
