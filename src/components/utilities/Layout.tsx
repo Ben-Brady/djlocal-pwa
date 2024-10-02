581c9108a7334542b0bb16b22d4290c22013b0cb
Ben Brady <benbrady04.bb@gmail.com>
2024-10-02 21:59:01 +0100

    feat: fixed overflow

--- src/components/utilities/Layout.tsx
+++ src/components/utilities/Layout.tsx
@@ -2,13 +2,14 @@ import { FC } from "preact/compat";
 import MinimizedPlayer from "@/components/extensive/MinimizedPlayer";
 import Navbar from "@/components/extensive/Navbar";
 import { useLocation } from "preact-iso";
+
 const Layout: FC = ({ children }) => {
     const { path } = useLocation();
 
     return (
         <div class="grid h-screen grid-rows-[1fr,64px] overflow-hidden">
-            <div className="relative h-[calc(100dvh-64px)] overflow-hidden">
-                {children}
+            <div className="relative h-[calc(100dvh-64px)]">
+                <div class="h-full overflow-auto">{children}</div>
                 {path !== "/queue" && <MinimizedPlayer />}
             </div>
             <Navbar />
